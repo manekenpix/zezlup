c840e4bd42414823e8ef4269101a816313ecc64a
Josue <manekenpix@fastmail.com>
2021-06-09 00:15:09 -0400

    refactor: remove unused dependencies

--- src/include/Window.h
+++ src/include/Window.h
@@ -1,11 +1,9 @@
 #ifndef WINDOW_HPP
 #define WINDOW_HPP
 
-#include <GL/glew.h>
+// #include <GL/glew.h>
+#include "glew.h"
 #include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include "types.hpp"
 
